5a9d0506ab966f580bffb83f03a2082b42d6b9f0
Syed Taqi <taqisyed141@gmail.com>
2024-10-05 12:28:51 +0500

    Added Main class to test Library functions

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+ {
+    public static void main(String[] args)
+     {
         Library myLibrary = new Library();
         myLibrary.addBook(&quot;Harry Potter&quot;);
         myLibrary.addBook(&quot;Lord of the Rings&quot;);
